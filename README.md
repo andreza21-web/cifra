e700fc47d98567603ef410115c3d3e8920b7f87d
Andreza <andreza.21pessoa@gmail.com>
2021-11-01 17:52:41 -0300

    cifra de cesar

---
+++ README.md
@@ -0,0 +1,32 @@
+# Segurança da Informação
+
+
+## Integrantes do grupo
+
+  Andreza do Nascimento Pessoa  Matricula: Cb3012191
+
+  Erica Barbosa Pereira   Matricula: CB3012701
+
+  Lidiane Souza Soares   Matricula: CB3012026
+
+
+
+# Regras para a realização da atividade:
+
+A atividade deve ser realizada pelo mesmo grupo de Trilhas em Segurança da Informação.
+
+Cada grupo deve realizar as seguinte tarefas:
+
+1. Escolher uma cifra criptográfica.
+  * Cifra de Cesar
+  * ROT13
+  * ROT47
+  * Cifra de Vigenère
+  * Cifra de Gronsfeld
+
+2. Construir uma interface simples que receba dados via teclado (input).
+3. Exibir os dados e os dados criptografados em tela (output).
+4. A seguir, exibir em tela uma relação de cifras criptográficas (entre as quais deve constar aquela escolhida pelo grupo no item 1).
+5. Solicitar ao usuário que responda "QUAL FOI A CIFRA CRIPTOGRÁFICA UTILIZADA PARA CRIPTOGRAFAR OS DADOS?".
+6. Após responder, o usuário será informado com "SIM" ou "NÃO, TENTE NOVAMENTE".
+7. Esgotadas todas as possibilidades, a rotina de execução deve ser encerrada com a mensagem "PARABÉNS!"
---
+++ src/codificar.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class codificar {
+    public static void main(String[] args) throws Exception {
+		Boolean menu = false;
+		int  numero;
+		int cifra;
+      
+		System.out.println("Digite sua mensagem:  ");
+		
+		Scanner input = new Scanner(System.in);
+		String msg = input.nextLine();
+		char[] caracteres = msg.toCharArray();
+
+		System.out.println("Digite o valor da cifra: ");
+		 cifra = input.nextInt();
+
+		for (int i = 0; i < caracteres.length; i++) {
+			int ascii = (int) caracteres[i] + cifra;
+			System.out.print((char) ascii);
+		}
+        
+		System.out.println("");
+		System.out.println("QUAL FOI A CIFRA CRIPTOGRÁFICA UTILIZADA PARA CRIPTOGRAFAR OS DADOS? ");
+		System.out.println("1. Cifra de Cesar");
+		System.out.println("2. ROT13");
+		System.out.println("3. ROT47");
+		System.out.println("4. Cifra de Vigenère");
+		System.out.println("5. Cifra de Gronsfeld");
+          
+            while (!menu)
+            {
+				do
+                {
+					System.out.println("Digite a resposta: ");
+					numero = input.nextInt();
+                } while( numero < 1 || numero > 5);
+
+                switch (numero)
+                {
+                    case 1:
+					System.out.println("Resposta correta, Parabéns!");
+                    menu = true;	
+                        break;
+
+                    case 2:
+                    System.out.println("NÃO, TENTE NOVAMENTE");
+                        break;
+
+                    case 3:
+                    System.out.println("NÃO, TENTE NOVAMENTE");
+                        break;
+
+                    case 4:
+                    System.out.println("NÃO, TENTE NOVAMENTE");
+                        break;
+
+                    case 5:
+                    System.out.println("NÃO, TENTE NOVAMENTE");
+                        break;
+                }
+            }
+
+        input.close();
+
+	}
+}
